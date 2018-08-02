96e0e41b1e40596374eabd69d0c2b2429eaa68f3
varsha <wangshuonpu@163.com>
2018-08-02 15:57:21 +0800

    重构文件结构；更换icon

---
+++ src/apiSuggest.js
@@ -0,0 +1,30 @@
+/**
+ * @file api suggester
+ * @author varsha
+ */
+
+const path = require('path');
+const fs = require('fs');
+const vscode = require('vscode');
+
+const typeDefine = require('./typeDefine');
+const referenceGenerate = require('./referenceGenerate');
+
+module.exports = () => {
+    // 工作目录读取失败时，不支持 api 提示
+    if (!vscode.workspace.rootPath) {
+        return Promise.reject();
+    }
+    // 工作根目录下没有 project.swan.json 认为不是小程序项目，不支持 api 提示
+    let jsonFilePath = path.join(vscode.workspace.rootPath, 'project.swan.json');
+    if (!fs.existsSync(jsonFilePath)) {
+        return Promise.reject();
+    }
+
+    return Promise.all([
+        // 安装swan.d.ts
+        typeDefine(),
+        // 创建swan.d.ts启动方式
+        referenceGenerate()
+    ]);
+};
