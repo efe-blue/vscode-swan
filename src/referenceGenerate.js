0a461211415bbdc4802b4711ec6b41614ba3b42d
zhuyeqing <zhuyeqing@baidu.com>
2018-08-03 11:46:22 +0800

    move variable declaration and definition

--- src/referenceGenerate.js
+++ src/referenceGenerate.js
@@ -8,12 +8,13 @@ const vscode = require('vscode');
 
 
 // 方式一：使用复制jsconfig.json的方式
-// 方式二：使用复制swan.d.ts到项目根目录即采用reference的启动方式（）
+// 方式二：使用复制swan.d.ts到项目根目录即采用reference的启动方式
 // 使用方式二
-const referenceSrcFile = path.join(__dirname, '../swan.d.ts');
-const referenceDestFile = path.join(vscode.workspace.rootPath, 'swan.d.ts');
 
-module.exports = () => fs.readFile(referenceSrcFile, (err, data) => {
+module.exports = () => {
+    let referenceSrcFile = path.join(__dirname, '../swan.d.ts');
+    let referenceDestFile = path.join(vscode.workspace.rootPath, 'swan.d.ts');
+    return fs.readFile(referenceSrcFile, (err, data) => {
         if (err) {
             vscode.window.showErrorMessage(`读取swan.d.ts配置文件失败: ${err}`);
         }
@@ -23,3 +24,4 @@ module.exports = () => fs.readFile(referenceSrcFile, (err, data) => {
             }
         });
     });
+};
