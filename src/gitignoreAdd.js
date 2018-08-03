8b13f90bebadb296e949a28886dcd5a5a226b856
zhuyeqing <zhuyeqing@baidu.com>
2018-08-03 20:49:37 +0800

    fix comments

--- src/gitignoreAdd.js
+++ src/gitignoreAdd.js
@@ -1,13 +1,18 @@
 /**
  * @file gitignore add
  * @author zhuyeqing
- * @param {string} data
  */
 
 const fs = require('fs');
 const path = require('path');
 const vscode = require('vscode');
 
+/**
+ * 为.gitignore增加文件或者文件夹
+ *
+ * @param {string} data 添加到.gitignore中的文件或者文件夹
+ * @return {Object}
+ */
 module.exports = data => {
     let gitignorePath = path.join(vscode.workspace.rootPath, '.gitignore');
     return fs.open(gitignorePath, 'r+', (err, fd) => {
