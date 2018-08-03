a263263707c723d9c77939832823e17d82afd463
zhuyeqing <zhuyeqing@baidu.com>
2018-08-03 20:11:42 +0800

    fix indent

--- src/gitignoreAdd.js
+++ src/gitignoreAdd.js
@@ -9,16 +9,16 @@ const path = require('path');
 const vscode = require('vscode');
 
 module.exports = data => {
-        let gitignorePath = path.join(vscode.workspace.rootPath, '.gitignore');
-        return fs.open(gitignorePath, 'r+', (err, fd) => {
+    let gitignorePath = path.join(vscode.workspace.rootPath, '.gitignore');
+    return fs.open(gitignorePath, 'r+', (err, fd) => {
+        if (err) {
+            return;
+        }
+        fs.write(fd, data, err => {
             if (err) {
                 return;
             }
-            fs.write(fd, data, err => {
-                if (err) {
-                    return;
-                }
-                fs.closeSync(fd);
-            });
+            fs.closeSync(fd);
         });
-    };
+    });
+};
