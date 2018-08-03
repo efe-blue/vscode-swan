5b1782a8299d744ac85b650d0b0af234f375e729
zhuyeqing <zhuyeqing@baidu.com>
2018-08-03 19:45:15 +0800

    add gitignore of typings

---
+++ src/gitignoreAdd.js
@@ -0,0 +1,24 @@
+/**
+ * @file gitignore add
+ * @author zhuyeqing
+ * @param {string} data
+ */
+
+const fs = require('fs');
+const path = require('path');
+const vscode = require('vscode');
+
+module.exports = data => {
+        let gitignorePath = path.join(vscode.workspace.rootPath, '.gitignore');
+        return fs.open(gitignorePath, 'r+', (err, fd) => {
+            if (err) {
+                return;
+            }
+            fs.write(fd, data, err => {
+                if (err) {
+                    return;
+                }
+                fs.closeSync(fd);
+            });
+        });
+    };
