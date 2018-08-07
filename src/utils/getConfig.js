11b00acefe720ef41149bea52986d2c8b7e98501
zhuyeqing <zhuyeqing@baidu.com>
2018-08-07 11:36:41 +0800

    modify input parameter

--- src/utils/getConfig.js
+++ src/utils/getConfig.js
@@ -5,17 +5,21 @@
 
 const vscode = require('vscode');
 
-// 可配置项描述
-// 1. disableAPISuggestion：默认值为false，用户可通过此配置开启或关闭 api 的提醒功能
+ /**
+  * 默认配置项
+  */
+const defaultConf = {
+    // 默认值为false，用户可通过此配置开启或关闭 api 的提醒功能
+    disableAPISuggestion: false
+};
 
-/**
+ /**
  * 获得配置项的值
  *
  * @param {string} configName 配置项名称
- * @param {any} defaultVal 配置项默认值
- * @return {any} 配置项值
+ * @return {Mixed} 配置项值
  */
-module.exports = (configName, defaultVal) => {
+module.exports = configName => {
     let swanConfig = vscode.workspace.getConfiguration('vscode-swan');
-    return swanConfig.get(configName, defaultVal);
+    return swanConfig.get(configName, defaultConf[configName]);
 };
