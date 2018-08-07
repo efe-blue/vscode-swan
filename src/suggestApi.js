5c316415e90159428933fda08900c9c189ced42c
zhuyeqing <zhuyeqing@baidu.com>
2018-08-07 11:36:13 +0800

    modify input parameter

--- src/suggestApi.js
+++ src/suggestApi.js
@@ -3,8 +3,8 @@
  * @author varsha
  */
 
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const vscode = require('vscode');
 
 const defineType = require('./defineType');
@@ -18,24 +18,25 @@ module.exports = () => {
     if (!vscode.workspace.rootPath) {
         return Promise.reject();
     }
+
     // 工作根目录下没有 project.swan.json 认为不是小程序项目，不支持 api 提示
     let jsonFilePath = path.join(vscode.workspace.rootPath, 'project.swan.json');
     if (!fs.existsSync(jsonFilePath)) {
         return Promise.reject();
     }
 
-    // 用户未禁用 api 提示
-    if (!getConfig('disableAPISuggestion', false)) {
-        return Promise.all([
-            // 安装swan.d.ts
-            defineType(),
-            // 创建swan.d.ts启动方式
-            addJsconfig(),
-            // 添加.gitignore
-            addGitignore('typings\n')
-        ]);
+    // 用户禁用了 api 提示
+    if (getConfig('disableAPISuggestion')) {
+        return Promise.reject();
     }
 
-    // 用户禁用了 api 提示
-    return Promise.reject();
+    // 用户未禁用 api 提示
+    return Promise.all([
+        // 安装swan.d.ts
+        defineType(),
+        // 创建swan.d.ts启动方式
+        addJsconfig(),
+        // 添加.gitignore
+        addGitignore('typings\n')
+    ]);
 };
